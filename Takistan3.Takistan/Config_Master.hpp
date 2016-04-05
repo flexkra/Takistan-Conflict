8941863e62badfa5675cb248d5c4ea3cefec9ca9
flexkra <felix.krause@udo.edu>
2016-04-05 12:37:07 +0200

    rent

--- Takistan3.Takistan/Config_Master.hpp
+++ Takistan3.Takistan/Config_Master.hpp
@@ -70,7 +70,7 @@ class Life_Settings {
     civ_skins = false; //Enable or disable civilian skins. Before enabling, you must add all the SEVEN files to textures folder. (It must be named as: civilian_uniform_1.jpg, civilian_uniform_2.jpg...civilian_uniform_6.jpg, civilian_uniform_7.jpg)
 
     /* Car-shop Settings */
-    vehicleShop_rentalOnly[] = { "B_G_Offroad_01_armed_F","B_MRAP_01_hmg_F" };
+    vehicleShop_rentalOnly[] = { "B_G_Offroad_01_armed_F","B_MRAP_01_hmg_F","O_MRAP_02_F","I_mas_cars_UAZ_M2","B_mas_cars_Hilux_M2","I_mas_cars_LR_soft_M2","I_mas_cars_UAZ_MG","B_mas_cars_Hilux_MG","I_mas_cars_UAZ_SPG9","B_mas_cars_Hilux_SPG9"};
 
     /* Job-related stuff */
     delivery_points[] = { "dp_1", "dp_2", "dp_3", "dp_4", "dp_5", "dp_6", "dp_7", "dp_8", "dp_9", "dp_10", "dp_11", "dp_12", "dp_13", "dp_14", "dp_15", "dp_15", "dp_16", "dp_17", "dp_18", "dp_19", "dp_20", "dp_21", "dp_22", "dp_23", "dp_24", "dp_25" };
